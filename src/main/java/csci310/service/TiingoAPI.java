bb6c51d49a327ad4c20249e889165a84e97ba5c5
Vikash Mody <vikasham@usc.edu>
2020-10-16 02:42:56 -0700

    added implementation for stockprice

--- src/main/java/csci310/service/TiingoAPI.java
+++ src/main/java/csci310/service/TiingoAPI.java
@@ -1,9 +1,41 @@
 package csci310.service;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
+
 public class TiingoAPI {
 	
-	public double stockPrice(String ticker) {
-		return 0;
+	private String token = "dfa3aad6d8290db5ee85f160d54a60a7bbc03e1c";
+	
+	public double stockPrice(String ticker) throws IOException {
+		
+		URL url = new URL("https://api.tiingo.com/tiingo/daily/" + ticker + "/prices?token=" + token);
+		HttpURLConnection con = (HttpURLConnection)url.openConnection();
+		con.setRequestMethod("GET");
+		con.setDoOutput(true);
+		con.setRequestProperty("Content-Type", "application/json");
+		InputStream is = con.getInputStream();
+		BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+		StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+		String line;
+		while ((line = rd.readLine()) != null) {
+		   response.append(line);
+		   response.append('\r');
+		}
+		rd.close();
+		
+		response.deleteCharAt(0);
+		response.deleteCharAt(response.length() - 1);
+		JSONObject myResponse = new JSONObject(response.toString());
+		
+		return myResponse.getDouble("close");
 	}
 
 }
--- src/test/java/csci310/service/TiingoAPITest.java
+++ src/test/java/csci310/service/TiingoAPITest.java
@@ -2,12 +2,14 @@ package csci310.service;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class TiingoAPITest {
 
 	@Test
-	public void testStockPrice() {
+	public void testStockPrice() throws IOException {
 		TiingoAPI stock = new TiingoAPI();
 		double price = stock.stockPrice("AAPL");
 		assertTrue(price > 50);
