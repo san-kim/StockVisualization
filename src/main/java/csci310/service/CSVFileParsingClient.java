bc575da2692c1cb7061cf35c382e0fabf954459f
cathleenyangg <cathleey@usc.edu>
2020-10-16 17:53:53 -0700

    TDD: Added helper method shells + corresponding tests

--- src/main/java/csci310/service/CSVFileParsingClient.java
+++ src/main/java/csci310/service/CSVFileParsingClient.java
@@ -1,9 +1,37 @@
 package csci310.service;
 
+import csci310.model.Stock;
+
 public class CSVFileParsingClient {
+
+	public CSVFileParsingClient() {
+		
+	}
 	
-	public int parseCSVFile(String filepath) {
+	/**
+	 * This function will read from the given file (provided it is found) and return either negative error
+	 * code if the CSV file is malformed in anyway, or a zero to indicate the file was successfully
+	 * read & added to the current signed in user's Portfolio
+	 * @param fileName
+	 * @return int code representing specific error or success
+	 */
+	public int parseCSVFile(String filename) {
 		return 0;
 	}
-
+	
+	/**
+	 * This helper function will read from the given file (provided it is found) and return an 
+	 * array of Strings, with each spot in the array holding one line from the file
+	 * @param fileName
+	 * @return array of Strings, each String is one line from the file.
+	 */
+	public String[] readFile(String fileName) {
+		String[] array = null;
+		return array;
+	}
+	
+	public Stock parseStock(String data) {
+		return null;
+	}
+	
 }
--- src/test/java/csci310/service/CSVFileParsingClientTest.java
+++ src/test/java/csci310/service/CSVFileParsingClientTest.java
@@ -5,12 +5,14 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import csci310.model.Stock;
+
 public class CSVFileParsingClientTest {
 	
 	private static CSVFileParsingClient parser;
 	
 	@BeforeClass 
-	public void setUp() {
+	public static void setUp() {
 		parser = new CSVFileParsingClient();
 	}
 
@@ -18,6 +20,18 @@ public class CSVFileParsingClientTest {
 	public void parseCSVFileTest() {
 		String filename = "bulkStocks.csv";
 		assertTrue(parser.parseCSVFile(filename) == 0);
+	}
+	
+	@Test
+	public void readFileTest() {
+		String filename = "bulkStocks.csv";
+		assertTrue(parser.readFile(filename) == null);
+	}
+	
+	@Test
+	public void parseStockTest() {
+		String dataMissingTicker = "Apple Inc,21,946368000,1609142400";
+		assertTrue(parser.parseStock(dataMissingTicker) == null);
 	}
 
 }
